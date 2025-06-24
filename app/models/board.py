ab413d27869dfb9adeb7949fc4c371e7ed8e0049
Brian Martinez <100006298+bamart-dev@users.noreply.github.com>
2025-06-24 10:33:57 -0700

    update Board model to include cards attribute, update Card model to include board_id & board attributes, implement to_dict and from_dict methods for Card model

--- app/models/board.py
+++ app/models/board.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from ..db import db
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
@@ -9,6 +9,8 @@ class Board(db.Model):
     board_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
     owner: Mapped[str]
+    cards: Mapped[list["Card"]] = relationship(back_populates="board")
+
 
     def to_dict(self):
         board = {
@@ -18,6 +20,7 @@ class Board(db.Model):
         }
 
         return board
+
 
     @classmethod
     def from_dict(cls, board_data):
--- app/models/card.py
+++ app/models/card.py
@@ -1,8 +1,34 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from ..db import db
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
+from app.db import db
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from .board import Board
 
 
 class Card(db.Model):
     card_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     message: Mapped[str]
     likes_count: Mapped[int]
+    board_id: Mapped[int] = mapped_column(ForeignKey("board.id"))
+    board: Mapped["Board"] = relationship(back_populates="cards")
+
+
+
+    def to_dict(self):
+        card = {
+            "id": self.card_id,
+            "message": self.message,
+            "likes": self.likes_count,
+        }
+
+        return card
+
+
+    @classmethod
+    def from_dict(cls, card_data):
+        return cls(
+            message = card_data["message"],
+            likes_count = card_data.get("likes", 0),
+            board_id = card_data["board_id"],
+        )
