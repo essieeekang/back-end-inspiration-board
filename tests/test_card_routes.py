70d57b0b3a65ce9fa70902c827bb86594bd666ed
Brian Martinez <100006298+bamart-dev@users.noreply.github.com>
2025-07-09 16:15:49 -0700

    removed s3 test

--- tests/test_card_routes.py
+++ tests/test_card_routes.py
@@ -129,26 +129,6 @@ def test_post_new_card(client, one_board):
     }]
 
 
-def test_post_new_card_with_image(client, one_board, mock_upload, mock_image_file):
-    bucket = os.environ.get("S3_BUCKET_NAME")
-    mock_upload.return_value = f"https://{bucket}.s3.amazonaws.com/test-image.jpg"
-
-    post_response = client.post("/cards", data={
-        "message": "Card with test image",
-        "board_id": 1,
-        "image": (mock_image_file, "test_image.jpg", "image/*"),
-    },
-    content_type="multipart/form-data")
-
-    get_response = client.get("/cards")
-    get_response_body = get_response.get_json()
-    print(get_response_body[0])
-    assert post_response.status_code == STATUS_CODE["CREATED"]
-    assert len(get_response_body) == 1
-    assert get_response_body[0]["image"] == f"https://{bucket}.s3.amazonaws.com/test-image.jpg"
-    mock_upload.assert_called_once()
-
-
 # PATCH route test
 def test_likes_count_increment(client, one_board, single_card):
     response = client.patch("/cards/1/like")
